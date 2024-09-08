23d9c24dbd57ea06f5ef8962e774d8453101fcb0
asommar <a.sommar@ya.ru>
2024-09-08 11:19:33 +0300

    add tests for nested

--- tests/DifferTest.php
+++ tests/DifferTest.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Differ\Tests;
+namespace Tests;
 
 use PHPUnit\Framework\TestCase;
 
@@ -8,27 +8,30 @@ use function Differ\Differ\genDiff;
 
 class DifferTest extends TestCase
 {
-    public function getFixtureFullPath($fixtureName): string
+    public function getFixtureFullPath(string $fixtureName): string
     {
         $parts = [__DIR__, 'fixtures', $fixtureName];
         return realpath(implode('/', $parts));
     }
 
-    public function testGenDiff(): void
+    public function testGenDiffFlat(): void
     {
-        $diff = implode("\n", [
-            "{",
-            " - follow: false",
-            "   host: hexlet.io",
-            " - proxy: 123.234.53.22",
-            " - timeout: 50",
-            " + timeout: 20",
-            " + verbose: true",
-            "}"
-        ]);
+        $diff = file_get_contents($this->getFixtureFullPath("flatDiff.txt"));
         $actual1 = genDiff($this->getFixtureFullPath("flat1.json"), $this->getFixtureFullPath("flat2.json"));
         $this->assertEquals($diff, $actual1);
         $actual2 = genDiff($this->getFixtureFullPath("flat1.yml"), $this->getFixtureFullPath("flat2.yaml"));
         $this->assertEquals($diff, $actual2);
+        $actual3 = genDiff($this->getFixtureFullPath("flat1.yml"), $this->getFixtureFullPath("flat2.json"));
+        $this->assertEquals($diff, $actual3);
+    }
+    public function testGenDiffNested(): void
+    {
+        $diff = file_get_contents($this->getFixtureFullPath("nestedDiff.txt"));
+        $actual1 = genDiff($this->getFixtureFullPath("nested1.json"), $this->getFixtureFullPath("nested2.json"));
+        $this->assertEquals($diff, $actual1);
+        $actual2 = genDiff($this->getFixtureFullPath("nested1.yaml"), $this->getFixtureFullPath("nested2.yaml"));
+        $this->assertEquals($diff, $actual2);
+        $actual3 = genDiff($this->getFixtureFullPath("nested1.json"), $this->getFixtureFullPath("nested2.yaml"));
+        $this->assertEquals($diff, $actual3);
     }
 }
