3616815cb06b2a75cbf1d627eec240be1733a859
asommar <159936732+asommar@users.noreply.github.com>
2025-03-01 17:36:59 +0300

    delete empty strings

--- tests/DifferTest.php
+++ tests/DifferTest.php
@@ -19,16 +19,12 @@ class DifferTest extends TestCase
         $diff = file_get_contents($this->getFixtureFullPath("{$format}.txt"));
         $jsonPath1 = $this->getFixtureFullPath("nested1.json");
         $jsonPath2 = $this->getFixtureFullPath("nested2.json");
-
         $actual1 = genDiff($jsonPath1, $jsonPath2, $format);
-
         $this->assertEquals($diff, $actual1);
 
         $yamlPath1 = $this->getFixtureFullPath("nested1.yaml");
         $yamlPath2 = $this->getFixtureFullPath("nested2.yaml");
-
         $actual2 = genDiff($yamlPath1, $yamlPath2, $format);
-
         $this->assertEquals($diff, $actual2);
 
         $actual3 = genDiff($jsonPath1, $yamlPath2, $format);
