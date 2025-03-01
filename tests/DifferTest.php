2a2e0bd2369839bd54e3b9cbf4a765821178ec37
asommar <a.sommar@ya.ru>
2025-03-01 20:56:49 +0300

    try more lines

--- tests/DifferTest.php
+++ tests/DifferTest.php
@@ -8,6 +8,26 @@ use function Differ\Differ\genDiff;
 
 class DifferTest extends TestCase
 {
+//    private function testGenDiff(string $format): void
+//    {
+//        $diff = file_get_contents($this->getFixtureFullPath("$format.txt"));
+//        $jsonPath1 = $this->getFixtureFullPath("file1.json");
+//        $jsonPath2 = $this->getFixtureFullPath("file2.json");
+//        $actual1 = genDiff($jsonPath1, $jsonPath2, $format);
+//        $this->assertEquals($diff, $actual1);
+//
+//        $yamlPath1 = $this->getFixtureFullPath("file1.yaml");
+//        $yamlPath2 = $this->getFixtureFullPath("file2.yaml");
+//        $actual2 = genDiff($yamlPath1, $yamlPath2, $format);
+//        $this->assertEquals($diff, $actual2);
+//
+//        $actual3 = genDiff($jsonPath1, $yamlPath2, $format);
+//        $this->assertEquals($diff, $actual3);
+//
+//        $actual4 = genDiff($yamlPath1, $jsonPath2, $format);
+//        $this->assertEquals($diff, $actual4);
+//    }
+
     private function testGenDiff(string $format): void
     {
         $diff = file_get_contents($this->getFixtureFullPath("$format.txt"));
