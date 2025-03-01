3229d7231cf080da8afd5cb19e5efa28f5baa055
asommar <a.sommar@ya.ru>
2025-03-01 23:17:15 +0300

    check another method name

--- tests/DifferTest.php
+++ tests/DifferTest.php
@@ -33,7 +33,7 @@ class DifferTest extends TestCase
         $actual4 = genDiff($yamlPath1, $jsonPath2, $format);
         $this->assertEquals($diff, $actual4);
     }
-    public function testGenDiff(): void
+    public function testDiffer(): void
     //public function testStylish(): void
     {
         $this->testFormat('stylish');
