3417cc9df430d49c64eac3fd480f024c4887c961
asommar <a.sommar@ya.ru>
2025-03-01 17:33:14 +0300

    add path variables

--- tests/DifferTest.php
+++ tests/DifferTest.php
@@ -17,26 +17,21 @@ class DifferTest extends TestCase
     private function testGenDiff(string $format): void
     {
         $diff = file_get_contents($this->getFixtureFullPath("{$format}.txt"));
+        $jsonPath1 = $this->getFixtureFullPath("nested1.json");
+        $jsonPath2 = $this->getFixtureFullPath("nested2.json");
 
-        $actual1 = genDiff(
-            $this->getFixtureFullPath("nested1.json"),
-            $this->getFixtureFullPath("nested2.json"),
-            $format
-        );
+        $actual1 = genDiff($jsonPath1, $jsonPath2, $format);
+
         $this->assertEquals($diff, $actual1);
 
-        $actual2 = genDiff(
-            $this->getFixtureFullPath("nested1.yaml"),
-            $this->getFixtureFullPath("nested2.yaml"),
-            $format
-        );
+        $yamlPath1 = $this->getFixtureFullPath("nested1.yaml");
+        $yamlPath2 = $this->getFixtureFullPath("nested2.yaml");
+
+        $actual2 = genDiff($yamlPath1, $yamlPath2, $format);
+
         $this->assertEquals($diff, $actual2);
 
-        $actual3 = genDiff(
-            $this->getFixtureFullPath("nested1.json"),
-            $this->getFixtureFullPath("nested2.yaml"),
-            $format
-        );
+        $actual3 = genDiff($jsonPath1, $yamlPath2, $format);
         $this->assertEquals($diff, $actual3);
     }
 
