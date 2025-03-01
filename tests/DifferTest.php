2f59af94d1ce2e0e5494f99322794a1563032e7e
asommar <a.sommar@ya.ru>
2025-03-01 21:06:35 +0300

    return everything to normal and fix lint

--- tests/DifferTest.php
+++ tests/DifferTest.php
@@ -8,7 +8,6 @@ use function Differ\Differ\genDiff;
 
 class DifferTest extends TestCase
 {
-
     private function getFixtureFullPath(string $fixtureName): string
     {
         $parts = [__DIR__, 'fixtures', $fixtureName];
